582a2fd895f0c0e16af5baed2d7e7b5b15994178
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2018-03-01 11:51:06 -0700

    Added imports to CTECTwitter and added data members to the constructor.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -2,20 +2,27 @@ package chat.model;
 
 import chat.controller.ChatbotController;
 
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.Twitter;
+import twitter4j.*;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.text.DecimalFormat;
+
 
 public class CTECTwitter
 {
 	private ChatbotController appController;
 	private Twitter chatbotTwitter;
+	private List<Status> searchedTweets;
+	private List<Status> tweetedWords;
+	private long totalWordCount;
 	
 	public CTECTwitter(ChatbotController appController)
 	{
 		this.appController = appController;
 		this.chatbotTwitter = TwitterFactory.getSingleton();
-		
+		this.tweetedWords = new ArrayList<Status>();
+		this.totalWordCount = 0;
 	}
 	public void sendTweet(String textToTweet)
 	{
